d41315a457016522168faca453781346c079d42d
User <User@DESKTOP-NMRQQJ1>
2023-07-03 14:52:57 +0300

    Add project files.

---
+++ DataAccessLayer/Concrate/Context.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrate
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-NMRQQJ1\\SQLEXPRESS; initial catalog=InspimoUowDB;integrated security=true");
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Process> Processes { get; set; }
+    }
+}
